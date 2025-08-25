cd729609d37e9747a7c355839839553b92716707
XzMayel <snjyaadii77@gmail.com>
2025-08-26 01:42:02 +0700

    Upload first version

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika Hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik luar sidebar ngilangin menu
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
